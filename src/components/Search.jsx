fcea8ec0f94e72817453a78c8f0fe1a2642c45e9
JayChavan-23 <jaychavan2223@gmail.com>
2025-05-26 13:55:14 +0930

    Search Done

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from "react";
 
-const Search = (props) => {
+const Search = ({ searchTerm, setSearchTerm }) => {
   return (
-    <div className='text-3xl text-white'>{props.searchTerm}</div>
-  )
-}
-export default Search
+    <div className="search">
+      <div>
+        <img src="search.svg" alt="search" />
+        <input
+          type="text"
+          placeholder="Search through thousands of movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+    </div>
+  );
+};
+export default Search;
