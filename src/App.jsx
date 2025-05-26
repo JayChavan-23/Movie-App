508aa860f853819e5924c665fcd711f32d9d41ec
JayChavan-23 <jaychavan2223@gmail.com>
2025-05-26 13:29:52 +0930

    Images Imported

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,14 @@ import "./App.css";
 
 const App = () => {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <h1>Find <span className="text-gradient">Movies</span>  you'll Enjoy Without the Hassle</h1>
+        </header>
+      </div>
+    </main>
   );
 };
 
