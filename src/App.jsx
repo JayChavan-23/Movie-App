6eb80191dac74b2a01c1a081fbf803c150fb9337
JayChavan-23 <jaychavan2223@gmail.com>
2025-06-04 02:10:23 +0930

    Search Function Added

--- src/App.jsx
+++ src/App.jsx
@@ -22,33 +22,37 @@ const App = () => {
   const [movieList, setMovieList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  const fetchMovies = async () => {
+  const fetchMovies = async (query = "") => {
     setIsLoading(true);
     setErrorMessage("");
     try {
-      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const endpoint = query
+        ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+        : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
       const response = await fetch(endpoint, API_OPTIONS);
       if (!response.ok) {
         throw new Error("Network response was not ok");
       }
       const data = await response.json();
-      if(data.response === 'False'){
-        setErrorMessage(data.Error || "An error occurred while fetching movies.");
+      if (data.response === "False") {
+        setErrorMessage(
+          data.Error || "An error occurred while fetching movies."
+        );
         setMovieList([]);
         return;
       }
       setMovieList(data.results || []);
-    } catch(error) {
+    } catch (error) {
       console.log(`Error fetching movies: ${error}`);
       setErrorMessage("An error occurred while fetching movies.");
-    }finally{
-        setIsLoading(false);
+    } finally {
+      setIsLoading(false);
     }
   };
 
   useEffect(() => {
-    fetchMovies();
-  }, []);
+    fetchMovies(searchTerm);
+  }, [searchTerm]);
   return (
     <main>
       <div className="pattern" />
@@ -64,18 +68,17 @@ const App = () => {
 
         <section className="all-movies">
           <h2 className="mt-[40px]">All Movies</h2>
-          {isLoading ?(
+          {isLoading ? (
             <Spinner />
-          ): errorMessage ? (
+          ) : errorMessage ? (
             <p className="text-red-500">{errorMessage}</p>
           ) : (
             <ul>
-              {movieList.map((movie) =>(
+              {movieList.map((movie) => (
                 <MovieCard key={movie.id} movie={movie} />
               ))}
             </ul>
           )}
-
         </section>
       </div>
     </main>
